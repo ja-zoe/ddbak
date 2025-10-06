4b4c97e18980f559ca176647b9ff777961771866
jazoe <j.vilfort01@gmail.com>
2025-10-06 16:35:38 -0400

    Change next config to proper hostname for images

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
-import { getServerURL } from "@/lib/utils";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['ddbak.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ddbak.vercel.app', // if your CMS serves images too
+        pathname: '/**',
+      },
+    ],
   },
 };
 
